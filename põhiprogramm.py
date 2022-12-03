e7f3cdbeaca76726fa95357910cc99affdadc66d
Anett Luigemaa <anett.luigemaa@gmail.com>
2022-12-03 13:39:25 +0200

    proov

--- põhiprogramm.py
+++ põhiprogramm.py
@@ -1,2 +1,23 @@
+pip install PySimpleGUI
 
-hihi
+import PySimpleGUI as sg
+
+sg.theme('Default1') #värv
+aken = [[sg.Text('Tere', size=(40,1), key='kasutaja_tekst')],
+[sg.Text('Sisesta klassi number, mida otsid', size=(40,1), key='arvuti_tekst')],
+[sg.Input(key='sisestatud_tekst')],
+[sg.Button('Otsi'), sg.Button('Lõpeta')]]
+
+aken = sg.Window('Vestlus', aknas)
+
+while True:
+    sündmus, väärtused = aken.read()
+    if sündmus == sg.WIN_CLOSED or sündmus == 'Lõpeta':
+        break
+    if sündmus == 'Otsi':
+        aken['arvuti_tekst'].update('See klassiruum asub siin')
+        aken['kasutaja_tekst'].update(väärtused['sisestatud_tekst'])
+        aken['sisestatud_tekst'].update('')
+        aken.open('sisestatud_tekst'+'.jpg')
+
+aken.close()
