0724e813b5306987d8784e4b485c647cda6a6202
Johanna Kasenurm <johanna.kasenurm@gmail.com>
2022-12-14 13:25:34 +0200

    See kuvab nüüd pilti, muidu veits katki veel

--- põhiprogramm.py
+++ põhiprogramm.py
@@ -1,7 +1,10 @@
 import PySimpleGUI as sg
+from PIL import Image
+import io, os
 
 sg.theme('Default1') #värv
 aken = [[sg.Text('Tere', size=(40,1), key='kasutaja_tekst')],
+[sg.Image(key='pilt')],
 [sg.Text('Sisesta klassi number, mida otsid', size=(40,1), key='arvuti_tekst')],
 [sg.Input(key='sisestatud_tekst')],
 [sg.Button('Otsi'), sg.Button('Lõpeta')]]
@@ -16,7 +19,18 @@ while True:
         aken['arvuti_tekst'].update('See klassiruum asub siin')
         aken['kasutaja_tekst'].update(väärtused['sisestatud_tekst'])
         aken['sisestatud_tekst'].update('')
-        aken.open('sisestatud_tekst'+'.jpg')
+       
+        faili_nimi = str(väärtused['sisestatud_tekst']) + '.jpg'
+        
+        #Kui selline fail eksisteerib
+        if os.path.exists(faili_nimi):
+            image = Image.open(faili_nimi)
+            #Määrab ära pildi suuruse pikslites
+            image.thumbnail((400, 400))
+            #Loob RAM'i koha, kuhu me saame pildi salvestada
+            bio = io.BytesIO()
+            #Salvestab pildi
+            image.save(bio, format='PNG')
+            aken['pilt'].update(data=bio.getvalue())
 
-#ja nii ongi
 aken.close()
